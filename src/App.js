d5b2fe5d450e6ee2365b5477e7043ec0be200146
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-08-09 20:50:48 -0500

    Added banner.js and called in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/banner";
 
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
+      
     </div>
   );
 }
